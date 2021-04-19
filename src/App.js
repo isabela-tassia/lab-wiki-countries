30dd03c3868d9d147a16266c28d89c5432473567
isabela tassia <isabelatassiasilva@gmail.com>
2021-04-18 23:03:29 -0300

    lab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+import Navbar from './components/NavBar';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+
+        <div className="container">
+          <div className="row">
+            <div className="col-5">
+              <CountriesList />
+            </div>
+
+            <div className="col-7">
+              <Route path="/:cca3" component={CountryDetails} />
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
